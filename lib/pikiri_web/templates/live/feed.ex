734254b43492deb649e6173dc02790abb638468c
Jeremy Rea <jeremyrea@fastmail.com>
2022-05-24 20:35:51 -0400

    Image dataset for testing

--- lib/pikiri_web/templates/live/feed.ex
+++ lib/pikiri_web/templates/live/feed.ex
@@ -6,28 +6,33 @@ defmodule PikiriWeb.Live.Feed do
     ~L"""
     <table>
       <tbody id="users" phx-update="append">
-        <%= for user <- @users do %>
+        <%= for {user, index} <- Enum.with_index(@users) do %>
           <tr id=<%= user %>>
-            <td><%= user %></td>
+            <td>
+                <%= if index == length(@users) - 1 do %>
+                    <div id="InfiniteScroll" phx-hook="InfiniteScroll" data-page="<%= @page %>"></div>
+                <% end %>
+                
+                <img src="/images/dataset/<%= user %>.jpg" style="max-height: 200px" />
+            </td>
           </tr>
         <% end %>
       </tbody>
     </table>
-    <div id="InfiniteScroll" phx-hook="InfiniteScroll" data-page="<%= @page %>"></div>
     """
   end
 
   def mount(_params, _session, socket) do
     {:ok,
      socket
-     |> assign(page: 1, per_page: 50)
+     |> assign(page: 1, per_page: 5)
      |> fetch(), temporary_assigns: [users: []]}
   end
 
   defp fetch(%{assigns: %{page: page, per_page: per}} = socket) do
     values = List.duplicate(nil, per) 
     |> Enum.with_index 
-    |> Enum.map fn {k,v} -> v+(50*(page-1)) end
+    |> Enum.map fn {k,v} -> v+(5*(page-1)) end
 
     assign(socket, users: values)
   end
