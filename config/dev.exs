0be2e73a200683f4f02e1ade24ca411b84e6c253
Jeremy Rea <jeremy@rea.email>
2022-12-17 01:55:58 -0500

    Ignore upload dir from live reload pattern

--- config/dev.exs
+++ config/dev.exs
@@ -54,7 +54,7 @@ config :pikiri, PikiriWeb.Endpoint,
 config :pikiri, PikiriWeb.Endpoint,
   live_reload: [
     patterns: [
-      ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
+      ~r"priv/static/(?!uploads).*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
       ~r"lib/pikiri_web/(live|views)/.*(ex)$",
       ~r"lib/pikiri_web/templates/.*(eex)$"
