ec7cfeeb3b69a17b85799fd17258e72b6d872d82
Jeremy Rea <jeremy@rea.email>
2022-12-17 23:49:27 -0500

    Comment unused config variable

--- config/runtime.exs
+++ config/runtime.exs
@@ -35,7 +35,7 @@ if config_env() == :prod do
   #     For example: ecto://USER:PASS@HOST/DATABASE
   #     """
 
-  maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
+  # maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
 
   # config :pikiri, Pikiri.Repo,
   #   # ssl: true,
