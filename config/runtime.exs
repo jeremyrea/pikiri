a95500cfb31091e81fbb925394968aefb2ea3b3b
Jeremy Rea <jeremy@rea.email>
2022-12-17 23:34:16 -0500

    Use sqlite config for prod

--- config/runtime.exs
+++ config/runtime.exs
@@ -28,20 +28,25 @@ admin_email =
 config :pikiri, admin_email: admin_email
 
 if config_env() == :prod do
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
+  # database_url =
+  #   System.get_env("DATABASE_URL") ||
+  #     raise """
+  #     environment variable DATABASE_URL is missing.
+  #     For example: ecto://USER:PASS@HOST/DATABASE
+  #     """
 
   maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
 
+  # config :pikiri, Pikiri.Repo,
+  #   # ssl: true,
+  #   url: database_url,
+  #   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  #   socket_options: maybe_ipv6
   config :pikiri, Pikiri.Repo,
-    # ssl: true,
-    url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+    database: "/app/db/pikiri.db",
+    stacktrace: true,
+    show_sensitive_data_on_connection_error: true,
+    pool_size: 10
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
