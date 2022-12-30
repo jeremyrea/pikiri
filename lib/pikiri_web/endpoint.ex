e8638f38ecf21fd9130463856a35941eceaed3c0
Jeremy Rea <jeremy@rea.email>
2022-12-30 01:59:39 -0500

    Change path again

--- lib/pikiri_web/endpoint.ex
+++ lib/pikiri_web/endpoint.ex
@@ -24,7 +24,7 @@ defmodule PikiriWeb.Endpoint do
 
   plug Plug.Static,
     at: "/uploads/posts",
-    from: Path.expand("./priv/static/uploads/posts"),
+    from: Path.expand("./uploads/posts"),
     only: ~w(public),
     gzip: false
 
