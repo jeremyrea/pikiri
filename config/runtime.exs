c10542d806a665c7398d085c7e97f470c9177631
Jeremy Rea <jeremy@rea.email>
2022-12-31 01:31:08 -0500

    Remove local port from production url

--- config/runtime.exs
+++ config/runtime.exs
@@ -59,7 +59,7 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :pikiri, PikiriWeb.Endpoint,
-    url: [host: host, port: port, scheme: "http"],
+    url: [host: host, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
